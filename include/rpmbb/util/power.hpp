e19f36ebdd9d4b191298ab696fe308f5afd4d4c0
range3 <white.line.oar@gmail.com>
2023-10-10 16:00:18 +0900

    [util] Add to_human(num) / from_human(str) / power<base, exp>()

---
+++ include/rpmbb/util/power.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+namespace rpmbb::util {
+
+template <uintmax_t base, unsigned int exp>
+constexpr auto power() -> uintmax_t {
+  if constexpr (exp == 0) {
+    return 1;
+  }
+  if constexpr (exp == 1) {
+    return base;
+  }
+
+  uintmax_t tmp = power<base, exp / 2>();
+  if constexpr (exp % 2 == 0) {
+    return tmp * tmp;
+  } else {
+    return base * tmp * tmp;
+  }
+}
+
+}  // namespace rpmbb::util
