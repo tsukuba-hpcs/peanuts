994b6b68cbd4ff0e983010354173574548a5fb77
range3 <white.line.oar@gmail.com>
2023-10-11 10:28:00 +0900

    [util] Fix missing #include

--- include/rpmbb/util.hpp
+++ include/rpmbb/util.hpp
@@ -4,6 +4,8 @@
 #include "rpmbb/util/delta_encoding.hpp"
 #include "rpmbb/util/enumerate.hpp"
 #include "rpmbb/util/gen_random_string.hpp"
+#include "rpmbb/util/human_readable.hpp"
+#include "rpmbb/util/power.hpp"
 #include "rpmbb/util/sense_barrier.hpp"
 #include "rpmbb/util/singleton.hpp"
 #include "rpmbb/util/stopwatch.hpp"
