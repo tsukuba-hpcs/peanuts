f7899623f51afb2755ca1fee72d083456e27b612
range3 <white.line.oar@gmail.com>
2023-10-17 16:20:15 +0900

    [core] Add rpmbb::topology

---
+++ include/rpmbb/topology.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "rpmbb/mpi.hpp"
+#include "rpmbb/utils/enumerate.hpp"
+
+#include <map>
+#include <ostream>
+#include <span>
+#include <tuple>
+#include <vector>
+
+namespace rpmbb {
+
+struct rank_pair {
+  int inter;
+  int intra;
+
+  bool operator==(const rank_pair& other) const {
+    return std::tie(inter, intra) == std::tie(other.inter, other.intra);
+  }
+
+  bool operator<(const rank_pair& other) const {
+    return std::tie(inter, intra) < std::tie(other.inter, other.intra);
+  }
+
+  std::ostream& inspect(std::ostream& os) const {
+    return os << "[" << inter << ", " << intra << "]";
+  }
+};
+
+class topology {
+ public:
+  explicit topology(const mpi::comm& comm)
+      : comm_(&comm),
+        intra_comm_{comm, mpi::split_type::shared},
+        inter_comm_{comm, intra_comm_.rank()},
+        rank_map_{create_rank_map()},
+        inverted_rank_map_{create_inverted_rank_map()} {}
+
+  auto comm() const -> const mpi::comm& { return *comm_; }
+  auto intra_comm() const -> const mpi::comm& { return intra_comm_; }
+  auto inter_comm() const -> const mpi::comm& { return inter_comm_; }
+
+  auto rank() const -> int { return comm_->rank(); }
+  auto size() const -> int { return comm_->size(); }
+  auto intra_rank() const -> int { return intra_comm_.rank(); }
+  auto intra_size() const -> int { return intra_comm_.size(); }
+  auto inter_rank() const -> int { return inter_comm_.rank(); }
+  auto inter_size() const -> int { return inter_comm_.size(); }
+  auto np() const -> int { return size(); }
+
+  auto global2inter_rank(const int global_rank) const -> int {
+    return rank_map_[global_rank].inter;
+  }
+
+  auto global2intra_rank(const int global_rank) const -> int {
+    return rank_map_[global_rank].intra;
+  }
+
+  auto global2rank_pair(const int global_rank) const -> rank_pair {
+    return rank_map_[global_rank];
+  }
+
+  auto inter2global_rank(const int inter_rank) const -> int {
+    return inverted_rank_map_.at({inter_rank, 0});
+  }
+
+  auto intra2global_rank(const int intra_rank) const -> int {
+    return inverted_rank_map_.at({inter_rank(), intra_rank});
+  }
+
+  auto rank_pair2global_rank(const rank_pair& pair) const -> int {
+    return inverted_rank_map_.at(pair);
+  }
+
+  auto rank_pair2global_rank(const int inter_rank, const int intra_rank) const
+      -> int {
+    return inverted_rank_map_.at({inter_rank, intra_rank});
+  }
+
+ private:
+  auto create_rank_map() const -> std::vector<rank_pair> {
+    std::vector<rank_pair> rank_map(size());
+    mpi::dtype rank_pair_dtype = mpi::dtype{mpi::to_dtype<int>(), 2};
+    rank_pair_dtype.commit();
+    const auto pair = rank_pair{inter_rank(), intra_rank()};
+    comm().all_gather(pair, rank_pair_dtype, std::span{rank_map},
+                      rank_pair_dtype);
+    return rank_map;
+  }
+
+  auto create_inverted_rank_map() const -> std::map<rank_pair, int> {
+    std::map<rank_pair, int> inverted_rank_map;
+    for (const auto& [global_rank, pair] : utils::cenumerate(rank_map_)) {
+      inverted_rank_map[pair] = global_rank;
+    }
+    return inverted_rank_map;
+  }
+
+ private:
+  const mpi::comm* comm_;
+  mpi::comm intra_comm_;
+  mpi::comm inter_comm_;
+  std::vector<rank_pair> rank_map_;
+  std::map<rank_pair, int> inverted_rank_map_;
+};
+
+}  // namespace rpmbb
---
+++ tests/topology_mpitest.cpp
@@ -0,0 +1,52 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/extensions/doctest_mpi.h"
+
+#include "rpmbb/inspector.hpp"
+#include "rpmbb/topology.hpp"
+using namespace rpmbb;
+
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+  // MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, nullptr);
+  doctest::mpi_init_thread(argc, argv, MPI_THREAD_MULTIPLE);
+
+  doctest::Context ctx;
+  ctx.setOption("abort-after", 5);
+  ctx.setOption("reporters", "MpiConsoleReporter");
+  // ctx.setOption("reporters", "MpiFileReporter");
+  ctx.setOption("force-colors", true);
+  ctx.applyCommandLine(argc, argv);
+
+  int test_result = ctx.run();
+
+  // MPI_Finalize();
+  doctest::mpi_finalize();
+
+  return test_result;
+}
+
+TEST_CASE("topology") {
+  rpmbb::topology topo{mpi::comm::world()};
+
+  // Validate basic rank and size operations
+  int world_rank = MPI_COMM_WORLD;
+  int world_size = MPI_COMM_WORLD;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  CHECK(topo.rank() == world_rank);
+  CHECK(topo.size() == world_size);
+  CHECK(topo.rank_pair2global_rank(topo.inter_rank(), topo.intra_rank()) ==
+        topo.rank());
+  CHECK(topo.global2intra_rank(topo.rank()) == topo.intra_rank());
+  CHECK(topo.global2inter_rank(topo.rank()) == topo.inter_rank());
+
+  if (topo.rank() == 0) {
+    auto intra_size = topo.intra_size();
+    if (topo.inter_size() > 1) {
+      CHECK(topo.rank_pair2global_rank(1, 0) == intra_size);
+      CHECK(topo.global2rank_pair(intra_size) == rank_pair{1, 0});
+    }
+  }
+}
