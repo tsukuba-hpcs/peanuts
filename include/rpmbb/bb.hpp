34eae0da6319f9a58ae9c6751c88cdc2ce93a2e4
range3 <white.line.oar@gmail.com>
2023-10-20 14:03:44 +0900

    [core] bb_store uses unorderd_set instead of unordered_map

--- include/rpmbb/bb.hpp
+++ include/rpmbb/bb.hpp
@@ -12,17 +12,33 @@
 
 namespace rpmbb {
 
-class bb {
+struct bb {
+  ino_t ino;
   extent_tree global_tree_{};
   extent_tree local_tree_{};
 };
+
+namespace detail {
+struct bb_hash {
+  std::size_t operator()(const std::shared_ptr<bb>& bb) const {
+    return std::hash<ino_t>()(bb->ino);
+  }
+};
+
+struct bb_equal {
+  bool operator()(const std::shared_ptr<bb>& lhs,
+                  const std::shared_ptr<bb>& rhs) const {
+    return lhs->ino == rhs->ino;
+  }
+};
+}  // namespace detail
 
 class bb_handler {
  public:
   bb_handler(std::shared_ptr<bb> bb,
              int fd,
              std::reference_wrapper<rpm> rpm_ref)
-      : bb_(std::move(bb)), fd_(fd), rpm_ref_(rpm_ref) {}
+      : bb_(std::move(bb)), fd_(std::move(fd)), rpm_ref_(rpm_ref) {}
 
  private:
   std::shared_ptr<bb> bb_;
@@ -35,22 +51,21 @@ class bb_store {
   bb_store(std::reference_wrapper<rpm> rpm_ref) : rpm_ref_(rpm_ref) {}
 
   std::unique_ptr<bb_handler> open(ino_t ino, int fd) {
-    auto it = bb_store_.find(ino);
-    if (it == bb_store_.end()) {
-      it = bb_store_.emplace(ino, std::make_shared<bb>()).first;
-    }
-    return std::make_unique<bb_handler>(it->second, fd, rpm_ref_);
+    auto bb_obj = std::make_shared<bb>(bb{ino});
+    auto [it, inserted] = bb_store_.insert(bb_obj);
+    return std::make_unique<bb_handler>(*it, std::move(fd), rpm_ref_);
   }
 
   std::unique_ptr<bb_handler> open(int fd) {
     return open(utils::get_ino(fd), fd);
   }
 
-  void unlink(ino_t ino) { bb_store_.erase(ino); }
+  void unlink(ino_t ino) { bb_store_.erase(std::make_shared<bb>(bb{ino})); }
   void unlink(int fd) { unlink(utils::get_ino(fd)); }
 
  private:
-  std::unordered_map<ino_t, std::shared_ptr<bb>> bb_store_;
+  std::unordered_set<std::shared_ptr<bb>, detail::bb_hash, detail::bb_equal>
+      bb_store_;
   std::reference_wrapper<rpm> rpm_ref_;
 };
 
