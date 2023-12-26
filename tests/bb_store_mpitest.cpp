e48e3bba7b04ede3c63dadc8f2e96da228ec3583
range3 <white.line.oar@gmail.com>
2023-12-26 16:20:52 +0900

    [tests] Adjusted file size calculation in bb_store test case.

--- tests/bb_store_mpitest.cpp
+++ tests/bb_store_mpitest.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
 TEST_CASE("bb_store") {
   topology topo{};
   rpm rpm{std::cref(topo), "/tmp/pmem2_devtest",
-          (1ULL << 21) * topo.intra_size()};
+          (2ULL << 20) * topo.intra_size()};
   auto store = bb_store{rpm};
 
   const auto files = std::vector<std::string>{"/tmp/bb_store_test_file1",
