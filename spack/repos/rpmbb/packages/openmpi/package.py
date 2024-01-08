2891c65e83124e5ea9aacb991485ae1877707b22
range3 <white.line.oar@gmail.com>
2024-01-08 18:53:10 +0900

    [spack] Add +pmembb and +debug to openmpi

--- spack/repos/rpmbb/packages/openmpi/package.py
+++ spack/repos/rpmbb/packages/openmpi/package.py
@@ -7,11 +7,18 @@ from spack.package import *
 from spack.pkg.builtin.openmpi import Openmpi as BuiltinOpenmpi 
 
 class Openmpi(BuiltinOpenmpi):
+
+    variant('pmembb', default=False, description='Support pmembb', when='+romio')
+    variant('debug', default=False, description='Enable debug')
+
     def configure_args(self):
         spec = self.spec
         config_args = super(Openmpi, self).configure_args()
-
         if spec.satisfies("@5:") and not spec.satisfies("schedulers=tm"):
             config_args.append("--without-pbs")
+        if spec.satisfies("+pmembb"):
+            config_args.append("--with-io-romio-flags=--with-file-system=testfs+ufs+pmembb")
+        if spec.satisfies("+debug"):
+            config_args.append("--enable-debug")
 
         return config_args
