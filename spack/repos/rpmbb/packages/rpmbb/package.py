01bbe3b2d7d79ec001bd9e527068e9c494c706c1
range3 <white.line.oar@gmail.com>
2024-01-26 15:56:21 +0900

    Release v0.10.1

--- spack/repos/rpmbb/packages/rpmbb/package.py
+++ spack/repos/rpmbb/packages/rpmbb/package.py
@@ -20,7 +20,7 @@ class Rpmbb(CMakePackage):
     variant("profiler", default=False, description="enable profiler")
 
     version("master", branch="master")
-    version("0.10.0", tag="v0.10.0")
+    version("0.10.1", tag="v0.10.1")
 
     depends_on("mpi")
     depends_on("pmdk+ndctl")
